4174d875f24c55bd26c9c86c0d791179614cbcab
Ronen Moldovan <ronen.moldovan@gmail.com>
2015-05-10 17:00:47 +0300

    Updated git repository

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Simple templates that shows loading templates on sections of the page',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/ronenm/meteor-with-loading',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
